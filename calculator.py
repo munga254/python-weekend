ba3b2a89d8738df8904e921716b40a6c78c614fe
munga254 <alfredmung21407@outlook.com>
2023-03-11 20:08:44 +0300

    what am doing

--- calculator.py
+++ calculator.py
@@ -1,4 +1,9 @@
-num1, operator, num2 = input("enter calculation: ").split()
+while True:
+    try:
+        num1, operator, num2 = input("enter calculation: ").split()
+        break
+    except  ValueError:
+        print("Syntax Error")
 #convert to interger
 num1 =int(num1)
 num2 =int(num2)
